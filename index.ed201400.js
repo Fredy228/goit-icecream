f251e02ae4c0d49ab2b578a7fae2fde808a212d0
Fredy228 <105521962+Fredy228@users.noreply.github.com>
2022-08-12 10:34:27 +0000

    Deploying to gh-pages from @ Fredy228/goit-icecream@fea8e0b5b0a706cbeaa74ea4bf9091620af3cd15 🚀

--- index.ed201400.js
+++
@@ -1,2 +0,0 @@
-$((function(){window.onscroll=function(){document.body.scrollTop>200||document.documentElement.scrollTop>200?document.getElementById("top-btn").style.display="block":document.getElementById("top-btn").style.display="none"}}));
-//# sourceMappingURL=index.ed201400.js.map
---
+++ index.fe8d80e1.js
@@ -0,0 +1,2 @@
+function scrollFunction(){document.body.scrollTop>200||document.documentElement.scrollTop>200?document.getElementById("top-btn").style.display="block":document.getElementById("top-btn").style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.fe8d80e1.js.map
