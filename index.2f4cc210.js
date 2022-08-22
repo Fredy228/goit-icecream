9343a1953e06c4f05176ae4f309ed43e8cce57ba
Fredy228 <105521962+Fredy228@users.noreply.github.com>
2022-08-22 19:50:50 +0000

    Deploying to gh-pages from @ Fredy228/goit-icecream@9530464dad5beceff5ab0f24d521eb3e4e17b747 🚀

---
+++ index.2f4cc210.js
@@ -0,0 +1,2 @@
+var history=document.querySelector("[data-history]"),readMoreBtn=document.querySelector("[data-read-more]"),readCloseBtn=document.querySelector("[data-read-close]");history.forEach((function(e){function t(){e.classList.toggle("is-more")}readMoreBtn.addEventListener("click",t),readCloseBtn.addEventListener("click",t)}));
+//# sourceMappingURL=index.2f4cc210.js.map
